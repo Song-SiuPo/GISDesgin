04ac25fc421ee5b914d1a083cfdf130a9c8e8d64
ToaruJ <44470754+ToaruJ@users.noreply.github.com>
2020-05-29 12:30:41 +0800

    添加新的图标

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace simpleGIS.Properties {
     // 类通过类似于 ResGen 或 Visual Studio 的工具自动生成的。
     // 若要添加或移除成员，请编辑 .ResX 文件，然后重新运行 ResGen
     // (以 /str 作为命令选项)，或重新生成 VS 项目。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -66,6 +66,16 @@ namespace simpleGIS.Properties {
         internal static System.Drawing.Bitmap DataAdd16 {
             get {
                 object obj = ResourceManager.GetObject("DataAdd16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap EditingVertex16 {
+            get {
+                object obj = ResourceManager.GetObject("EditingVertex16", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -156,6 +166,26 @@ namespace simpleGIS.Properties {
         internal static System.Drawing.Bitmap GenericSearch16 {
             get {
                 object obj = ResourceManager.GetObject("GenericSearch16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap PanTool_B_16 {
+            get {
+                object obj = ResourceManager.GetObject("PanTool_B_16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap SelectionSelectTool16 {
+            get {
+                object obj = ResourceManager.GetObject("SelectionSelectTool16", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
